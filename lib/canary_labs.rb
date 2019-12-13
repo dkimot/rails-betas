cd17dbc8523ab53e8142cae06e486cc85dcd2bb2
Davis Kimoto <davis.kimoto@gmail.com>
2019-12-13 11:42:43 -0700

    renamed stragglers and moved public API to module level

--- lib/canary_labs.rb
+++ lib/canary_labs.rb
@@ -1,2 +1,28 @@
 module CanaryLabs
+  mattr_accessor :default_participation
+
+  def self.setup default_participation: true
+    @@experiments = {}
+    @@default_participation = default_participation
+  end
+
+  def self.participating? feat_name, id
+    experiment = @@experiments[feat_name]
+
+    return @@default_participation if experiment.nil?
+
+    experiment.participating? id
+  end
+
+  def self.add name, desc, resolver
+    @@experiments[name] = Feature.new name, desc, resolver
+  end
+
+  def self.find name
+    @@experiments[name]
+  end
+
+  def self.all
+    @@experiments.values
+  end
 end
---
+++ test/canary_labs_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+require 'feature'
+require 'resolvers/group'
+
+class CanaryLabsTest < ActiveSupport::TestCase
+  setup do
+    CanaryLabs.setup
+  end
+
+  test "truth" do
+    assert_kind_of Module, CanaryLabs
+  end
+
+  def test_add_feature
+    CanaryLabs.add :added_feat, '', CanaryLabs::Resolvers::Group.new([])
+
+    assert_includes(
+      CanaryLabs.all,
+      Feature.new(:added_feat, '', CanaryLabs::Resolvers::Group.new([])),
+    )
+  end
+
+  def test_find_feature_by_name
+    CanaryLabs.add :feat_to_find, '', CanaryLabs::Resolvers::Group.new([])
+
+    assert_equal(
+      Feature.new(:feat_to_find, '', CanaryLabs::Resolvers::Group.new([])),
+      CanaryLabs.find(:feat_to_find),
+    )
+  end
+
+  def test_participating_by_name
+    CanaryLabs.add :feat, '', CanaryLabs::Resolvers::Group.new(['my_id'])
+
+    assert CanaryLabs.participating? :feat, 'my_id'
+  end
+
+  def test_participating_with_bad_name
+    assert CanaryLabs.participating? :bad_name, 'my_id'
+  end
+
+  def test_list_all_feats
+    CanaryLabs.add :feat_a, '', CanaryLabs::Resolvers::Group.new([])
+
+    all_feats = CanaryLabs.all
+
+    assert_includes all_feats, Feature.new(:feat_a, '', CanaryLabs::Resolvers::Group.new([]))
+  end
+end
