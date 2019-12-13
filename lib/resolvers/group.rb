98c1378407592e3f73932120a0c8c50264ad64f1
Davis Kimoto <davis.kimoto@gmail.com>
2019-12-13 06:48:28 -0700

    stop over engineering stuff

--- lib/resolvers/group.rb
+++ lib/resolvers/group.rb
@@ -2,16 +2,10 @@ module Betas
   module Resolvers
     class Group
       def initialize group
-        @group = group.map do |member|
-          member = member.downcase if member.is_a? String
-
-          member.to_sym
-        end
+        @group = group
       end
 
       def participating? id
-        id = id.downcase.to_sym if id.is_a? String
-
         @group.include? id
       end
     end
--- test/resolvers/group_test.rb
+++ test/resolvers/group_test.rb
@@ -5,12 +5,12 @@ module Betas
   module Resolvers
     class GroupTest < ActiveSupport::TestCase
       def test_participating
-        resolver = Group.new ['admin_id', :dev_id, :qa_id, 'beta_lover_id']
+        resolver = Group.new ['admin_id', "dev_id", "qa_id", 'beta_lover_id']
         id_participating_pairs = {
           'admin_id' => true,
-          :dev_id => true,
+          "dev_id" => true,
           'qa_id' => true,
-          :beta_lover_id => true,
+          "beta_lover_id" => true,
           'boring_customer_id' => false,
         }
 
