4a9db01797861797e6a6674888f90fbc49b3d916
Davis Kimoto <davis.kimoto@gmail.com>
2019-12-13 06:35:23 -0700

    convert symbol name to string value

--- lib/resolvers/percentage.rb
+++ lib/resolvers/percentage.rb
@@ -12,7 +12,7 @@ module Betas
         # create a deterministic number based on the item identifier and
         # name of the beta. This will evenly distribute exposure to beta's
         # across all ID's.
-        determ_num = Digest::MD5.hexdigest(id.to_s + @beta_name).to_i(16)
+        determ_num = Digest::MD5.hexdigest(id.to_s + @beta_name.to_s).to_i(16)
         (determ_num % (@percentage * 100).to_i).zero?
       end
     end
