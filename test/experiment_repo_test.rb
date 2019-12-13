526db4fd5546feb0a0feee85827fdecb47b28480
Davis Kimoto <davis.kimoto@gmail.com>
2019-12-13 07:07:26 -0700

    add test for experiment repo

---
+++ test/experiment_repo_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+require 'experiment_repo'
+require 'beta'
+require 'resolvers/group'
+
+module Betas
+  class ExperimentRepoTest < ActiveSupport::TestCase
+    def test_add_beta
+      repo = new_repo
+      repo.add :added_beta, "", Betas::Resolvers::Group.new([])
+
+      assert_includes(
+        Beta.new(:added_beta, "", Betas::Resolvers::Group.new([])),
+        repo.all,
+      )
+    end
+
+    def test_find_beta_by_name
+      repo = new_repo
+      repo.add :beta_to_find, "", Betas::Resolvers::Group.new([])
+
+      assert_equal(
+        Beta.new(:beta_to_find, "", Betas::Resolvers::Group.new([])),
+        repo.find(:beta_to_find)
+       )
+    end
+
+    def test_participating_by_name
+      repo = new_repo
+      repo.add :participatable_beta, "", Betas::Resolvers::Group.new(["my_id"])
+
+      assert repo.participating? :participatable_beta, "my_id"
+    end
+
+    def test_list_all_betas
+      repo = new_repo
+      repo.add :beta_a, "", Betas::Resolvers::Group.new([])
+      repo.add :beta_b, "", Betas::Resolvers::Group.new([])
+      repo.add :beta_c, "", Betas::Resolvers::Group.new([])
+
+      all_betas = repo.all
+
+      assert_equal 3, all_betas.length
+      assert_includes all_betas, Beta.new(:beta_a, "", Betas::Resolvers::Group.new([]))
+    end
+
+    private
+
+    def new_repo
+      Betas::ExperimentRepo.new
+    end
+  end
+end
