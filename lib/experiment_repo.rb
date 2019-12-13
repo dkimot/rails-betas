53cda65b92d3bc4d419a5a8a8ab07bbc18f7ed4b
Davis Kimoto <davis.kimoto@gmail.com>
2019-12-13 07:19:27 -0700

    update default participation behavior

--- lib/experiment_repo.rb
+++ lib/experiment_repo.rb
@@ -1,11 +1,18 @@
 module Betas
   class ExperimentRepo
-    def initialize
+    attr_reader :default_participation
+
+    def initialize default_participation: true
       @betas = {}
+      @default_participation = default_participation
     end
 
     def participating? beta_name, id
-      @betas[beta_name].participating? id
+      b = @betas[beta_name]
+
+      return @default_participation if b.nil?
+
+      b.participating? id
     end
 
     def add name, desc, resolver
